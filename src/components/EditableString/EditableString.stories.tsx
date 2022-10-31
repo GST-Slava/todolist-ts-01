6be60a6c96835e35749503ec90f706beba4cf822
GST-Slava <gstslava@gmail.com>
2022-10-31 23:53:22 +0300

    add stories EditableString.stories.tsx

---
+++ src/components/EditableString/EditableString.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+import {action} from "@storybook/addon-actions";
+import EditableString from "./EditableString";
+
+export default {
+    title: 'TODOLIST/EditableString',
+    component: EditableString,
+} as ComponentMeta<typeof EditableString>;
+
+
+const Template: ComponentStory<typeof EditableString> = (args) => <EditableString {...args} />;
+
+export const EditableStringExample = Template.bind({});
+EditableStringExample.args = {
+
+    changeTitle: action('Value EditableString changed ')
+};
+
